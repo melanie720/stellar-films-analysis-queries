94a7d5f592ace2132faf0c10921f857778b27b2d
Melanie Thomas <242290890+melanie720@users.noreply.github.com>
2026-05-18 00:33:57 -0400

    Update procedure

--- 2_stellar_films_analysis_queries.sql
+++ 2_stellar_films_analysis_queries.sql
@@ -132,15 +132,15 @@ Stored procedure that recommends a "Greenlight" decision to a producer.
 
 Approach:
 
-1. I used temporary tables to create the necessary behavior.
-    There were a couple of checks required throughout the operation that performed better as separate queries.
-    i.e. Filtering by row rankings; Searching for a provided parameter value in the results before returning.
+1. I used Views and CTEs for reusability and readability.
 
 2. I also used the cast(), format(), and string_agg() built-in functions.
     Here, string_agg() is useful when a genre has more than one optimal filming location.
+    
+3. Window functions, partitioning, aggregate functions, group by, JOINs.
 
 
-If a user supplies a director and a genre, the procedure checks:
+If a user supplies a director and a genre, the procedure provides:
 
 1.	The director’s ROI of each film.
         Column: film_rev_per_dollar_spent
@@ -155,6 +155,69 @@ If a user supplies a director and a genre, the procedure checks:
             The location that yielded the highest average profit of all hosted StellarFilms of the genre.   
 */
 
+
+-- drop view if exists genre_optimal_locations_avg_profit;
+
+-- This View shows you each genre along with its optimal filming location and average profit.
+    create view genre_optimal_locations_avg_profit
+as
+
+-- This first CTE takes each film and its genre, filming location, and financial information, then finds the average profit of each genre in each filming location.
+-- The average profits are ranked using dense_rank() so there are no skipped rankings (in case we want to see the 2nd highest average profits one day).
+-- The highest average profit (rank 1) identifies the optimal filming location.
+with genre_location_rankings as (
+    select 
+        g.genre_id,
+        g.genre_name,
+        l.location_name, 
+        avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) as avg_profit_gen_loc,
+        dense_rank() over (partition by g.genre_id order by avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) desc) as profit_ranking
+            from locations as l
+            join productions as p on p.primary_location_id = l.location_id
+            join genres as g on g.genre_id = p.genre_id
+            join finances as f on f.prod_id = p.prod_id
+                group by g.genre_id, g.genre_name, l.location_name
+),
+-- This CTE finds the average profit for each genre across all filming locations.
+genre_avg_profit as (
+    select 
+        distinct g.genre_id,
+        format(avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) over (partition by g.genre_name), 'C') as Avg_Profit_for_Genre
+            from finances as f
+            join productions as p on p.prod_id = f.prod_id
+            join genres as g on g.genre_id = p.genre_id
+)
+
+-- The following query joins the two CTEs by genre, creating a View that returns each genre with its optimal filming location(s) 
+    -- and the average profit for each genre across all filming locations.
+select 
+    l.genre_id, 
+    l.genre_name,
+    string_agg(l.location_name, ', ') as Optimal_Loc_for_Genre,
+    Avg_Profit_for_Genre
+        from genre_location_rankings as l
+        join genre_avg_profit as p on p.genre_id = l.genre_id
+        where profit_ranking = 1
+        group by l.genre_id, l.genre_name, p.Avg_Profit_for_Genre;
+
+
+-- drop view if exists director_film_stats;
+
+-- This view contains each director's revenue-per-dollar-spent for each of their films.
+create view director_film_stats
+as
+select 
+    distinct t.person_firstname as director_fn, 
+    t.person_lastname as director_ln, 
+    p.title as Title, 
+    g.genre_name as Genre,
+    cast(f.box_office_global/f.actual_spend as decimal(5,2)) as Film_Rev_per_Dollar_Spent
+        from talent_stats as t
+        right join productions as p on t.person_id = p.director_id
+        join genres as g on g.genre_id = p.genre_id
+        join finances as f on f.prod_id = p.prod_id;
+
+
 drop procedure if exists p_greenlight
 go 
 
@@ -163,101 +226,69 @@ create procedure p_greenlight (
     @director_lastname varchar(20) = '',
     @genre varchar(15) = ''
 ) as begin    
-    select g.genre_id, l.location_id, l.location_name, 
-        avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) as avg_profit_gen_loc,
-        dense_rank() over (partition by g.genre_id order by avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) desc) as profit_ranking
-            into #genre_location_profit
-            from locations as l
-            join productions as p on p.primary_location_id = l.location_id
-            join genres as g on g.genre_id = p.genre_id
-            join finances as f on f.prod_id = p.prod_id
-                group by g.genre_id, l.location_id, l.location_name
-
-    select genre_id, location_id, location_name
-        into #optimal_location
-        from #genre_location_profit
-            where profit_ranking = 1
-
-    select 
-        distinct t.person_firstname as director_fn, t.person_lastname as director_ln, 
-        p.title as Title, g.genre_name as Genre,
-        cast(f.box_office_global/f.actual_spend as decimal(5,2)) as Film_Rev_per_Dollar_Spent,
-        format((select distinct avg(f.box_office_global - (f.actual_spend + f.marketing_spend)) over (partition by g.genre_name)
-            from finances as f
-            join productions as p2 on p2.prod_id = f.prod_id
-            join genres as g on g.genre_id = p.genre_id
-                where p2.genre_id = p.genre_id
-        ), 'C') as Avg_Profit_for_Genre,
-        string_agg(o.location_name, ', ') as Optimal_Loc_for_Genre
-            into #director_genre_stats
-            from talent_stats as t
-            right join productions as p on t.person_id = p.director_id
-            join genres as g on g.genre_id = p.genre_id
-            join finances as f on f.prod_id = p.prod_id
-            join #optimal_location as o on o.genre_id = p.genre_id
-                group by t.person_firstname, t.person_lastname, p.title, g.genre_name, 
-                    f.box_office_global, f.actual_spend, p.genre_id
-
--- Wrong combination of director first and last name provided.
+    -- Checks for invalid combination of provided director first and last name.
     if @director_firstname != '' and @director_lastname != ''
-      if not exists (select 1 from talent_stats where person_lastname = @director_lastname and person_firstname = @director_firstname)
+        if not exists (select 1 from talent_stats where person_lastname = @director_lastname and person_firstname = @director_firstname)
         throw 50016, 'Director by that name does not exist.', 1
-    
--- Genre is provided.
-    if @genre != '' begin
-        -- Both first and last names provided.
-        if @director_firstname != '' and @director_lastname != '' begin
+
+    -- Both first and last names provided.
+    if @director_firstname != '' and @director_lastname != '' begin
+        -- Genre found in Director's filmography.
+        if exists (select 1 from director_film_stats where Genre = @genre and director_fn = @director_firstname and director_ln = @director_lastname) begin
             select 
                 director_fn + ' ' + director_ln as Director,
                 Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
-                    from #director_genre_stats
-                    where Genre = @genre and director_fn = @director_firstname and director_ln = @director_lastname
-                    order by Film_Rev_per_Dollar_Spent desc
-        end
-        -- First or last name provided.
-        else if @director_firstname != '' or @director_lastname != '' begin
-            select 
-                director_fn + ' ' + director_ln as Director,
-                Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
-                    from #director_genre_stats
-                    where Genre = @genre and (director_fn = @director_firstname or director_ln = @director_lastname)
+                    from director_film_stats
+                    join genre_optimal_locations_avg_profit on Genre = genre_name
+                    -- Handles genre provided and no genre provided.
+                    where (@genre = '' or Genre = @genre) and director_fn = @director_firstname and director_ln = @director_lastname
                     order by Director, Film_Rev_per_Dollar_Spent desc
         end
-        -- Only genre provided.
+        -- Genre not found in Director's filmography.
         else begin
             select 
                 director_fn + ' ' + director_ln as Director,
                 Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
-                    from #director_genre_stats
-                    where Genre = @genre
+                    from director_film_stats
+                    join genre_optimal_locations_avg_profit on Genre = genre_name
+                    where director_fn = @director_firstname and director_ln = @director_lastname
                     order by Director, Film_Rev_per_Dollar_Spent desc
         end
     end
--- No genre provided.
-    else begin
-        -- Both first and last names provided.
-        if @director_firstname != '' and @director_lastname != '' begin
+    -- First or last name provided.
+    else if @director_firstname != '' or @director_lastname != '' begin
+        -- Genre found in Director's filmography.
+        if exists (select 1 from director_film_stats where Genre = @genre and (director_fn = @director_firstname or director_ln = @director_lastname)) begin
             select 
                 director_fn + ' ' + director_ln as Director,
                 Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
-                    from #director_genre_stats
-                    where director_fn = @director_firstname and director_ln = @director_lastname
-                    order by Film_Rev_per_Dollar_Spent desc
+                    from director_film_stats
+                    join genre_optimal_locations_avg_profit on Genre = genre_name
+                    -- Handles genre provided and no genre provided.
+                    where (@genre = '' or Genre = @genre) and (director_fn = @director_firstname or director_ln = @director_lastname)
+                    order by Director, Film_Rev_per_Dollar_Spent desc
         end
-        -- First or last name provided.
+        -- Genre not found in Director's filmography.
         else begin
             select 
                 director_fn + ' ' + director_ln as Director,
                 Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
-                    from #director_genre_stats
+                    from director_film_stats
+                    join genre_optimal_locations_avg_profit on Genre = genre_name
                     where director_fn = @director_firstname or director_ln = @director_lastname
                     order by Director, Film_Rev_per_Dollar_Spent desc
         end
     end
-
-    drop table #genre_location_profit;
-    drop table #optimal_location;
-    drop table #director_genre_stats;
+    -- Only genre provided.
+    else begin
+        select 
+            director_fn + ' ' + director_ln as Director,
+            Title, Genre, Film_Rev_per_Dollar_Spent, Avg_Profit_for_Genre, Optimal_Loc_for_Genre
+                from director_film_stats
+                join genre_optimal_locations_avg_profit on Genre = genre_name
+                where Genre = @genre
+                order by Director, Film_Rev_per_Dollar_Spent desc
+    end
 end
 go
 
